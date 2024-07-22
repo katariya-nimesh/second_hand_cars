db3545532c9f7cec0e462b92ef6e4e62fd822518
Tushar <tusharbaldaniya2015@gmail.com>
2024-07-22 16:02:40 +0530

    re added changes

--- resources/views/admin/car-varients/edit.blade.php
+++ resources/views/admin/car-varients/edit.blade.php
@@ -11,6 +11,15 @@
             <input type="text" id="name" name="name" value="{{ $carVarient->name }}" required>
         </div>
         <div>
+            <label for="car_brand_id">Car Brand:</label>
+            <select id="car_brand_id" name="car_brand_id" required>
+                <option value="">Select Car Brand</option>
+                @foreach($carBrands as $brand)
+                    <option value="{{ $brand->id }}" {{ $carVarient->car_registration_year->car_brand_id == $brand->id ? 'selected' : '' }}>{{ $brand->name }}</option>
+                @endforeach
+            </select>
+        </div>
+        <div>
             <label for="car_registration_year_id">Registration Year:</label>
             <select id="car_registration_year_id" name="car_registration_year_id" required>
                 <option value="">Select Registration Year</option>
@@ -21,4 +30,41 @@
         </div>
         <button type="submit">Update</button>
     </form>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            // Load registration years when the page loads if a car brand is selected
+            var initialCarBrandId = $('#car_brand_id').val();
+            if (initialCarBrandId) {
+                loadRegistrationYears(initialCarBrandId);
+            }
+
+            // Load registration years when the car brand changes
+            $('#car_brand_id').on('change', function() {
+                var carBrandId = $(this).val();
+                loadRegistrationYears(carBrandId);
+            });
+
+            function loadRegistrationYears(carBrandId) {
+                $('#car_registration_year_id').empty();
+                if (carBrandId) {
+                    $.ajax({
+                        url: '{{ route('car-varients.registration-years') }}',
+                        type: 'GET',
+                        data: { car_brand_id: carBrandId },
+                        success: function(data) {
+                            $('#car_registration_year_id').append('<option value="">Select Registration Year</option>');
+                            $.each(data, function(key, value) {
+                                var selected = ({{ $carVarient->car_registration_year_id }} == value.id) ? 'selected' : '';
+                                $('#car_registration_year_id').append('<option value="' + value.id + '" ' + selected + '>' + value.year + '</option>');
+                            });
+                        }
+                    });
+                } else {
+                    $('#car_registration_year_id').append('<option value="">Select Registration Year</option>');
+                }
+            }
+        });
+    </script>
 @endsection
