b45b06c8d1f7400ac3a04d3b801a8374e3317983
Dinesh Parmar <dineshparmar26201@gmail.com>
2024-08-12 16:32:23 +0530

    Added plan migration and model

---
+++ database/migrations/2024_08_10_100932_create_plans_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('plans', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',50)->nullable();
+            $table->integer('total_cars')->nullable();
+            $table->float('price',10,2)->nullable();
+            $table->boolean('status')->default(1)->comment("1 = active,0 = deactive")->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plans');
+    }
+};
